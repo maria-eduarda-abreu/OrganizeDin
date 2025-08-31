dc9643bf964bdd480e385447a2b911faf5c688fa
unknown <mariaeduardaabreu005@gmail.com>
2025-08-31 19:56:03 -0300

    adicionando camada de repositório

---
+++ backend/src/main/java/model/User.java
@@ -0,0 +1,23 @@
+package com.organizedin.organize_din.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+}
---
+++ backend/src/main/java/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.organizedin.organize_din.repository;
+
+import com.organizedin.organize_din.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
